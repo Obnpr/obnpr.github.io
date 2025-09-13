99493e7e557f507e5fb6ab8b83625580b57fab26
Obed Negron Bodon <kaiserob@icloud.com>
2025-09-13 01:20:40 -0400

    Worked on both alignment and appearance of the  Home Page. Started and left About Me for later

--- code-atlus/src/components/AboutMe/AboutMe.tsx
+++ code-atlus/src/components/AboutMe/AboutMe.tsx
@@ -3,16 +3,15 @@ import './AboutMe.css';
 function AboutMe() {
   
   return (
-    <>
       <main className="main-content-box">
-        <div className="main-content">
+        <article className="aboutme-main-content">
           <h1>About Me</h1>
             <img 
-              className="titlesubimage"
+              className="aboutme-titlesubimage"
               src="/picture/profile-icon-3b.jpg"
               alt="A picture of Obed Negron"
             />
-          <div className="titlesubdiv">
+          <div className="aboutme-titlesubdiv">
               <p>
                 I’ve been an undergraduate Computer Science student for 5 (five) years now, and throughout that time my passion for technology has only deepened. Currently living in my hometown of Juana Diaz, Puerto Rico, I got my weak and old 2011 HP work desktop, it wasn’t powerful, but it slowly introduced me into a whole world later in my years.
               </p>
@@ -20,9 +19,8 @@ function AboutMe() {
                 Over the years, I’ve explored different areas of computer science, and creating not-so-useful applications for my own use. Recently, with my current interests are in game development and web design. But open to learn new programs and languages.
               </p>
           </div>
-        </div>
+        </article>
       </main>
-    </>
   );
 }
 
--- code-atlus/src/components/Home/Home.tsx
+++ code-atlus/src/components/Home/Home.tsx
@@ -7,10 +7,11 @@ function HomePage() {
 
   return (
     <main className="main-content-box">
-      <article>
-        <h1>I'm Obed Negrón, from the beautiful island of Puerto Rico!</h1>
-        <div className="titlesubdiv">
-            <p className="titlesubtext">Undergraduate student with a focus on software and front-end development. Experienced in computer hardware, video game development, and proficient in Linux systems, particularly Debian-based distributions.</p>
+      <article className="Intro-cont"> 
+          <h1>I'm Obed Negrón.</h1>
+          <h2>Front-end developer and game developer.</h2>
+          <div className="Intro-alignment"> 
+            <p>Undergraduate student with a focus on software and front-end development. Experienced in computer hardware, video game development, and proficient in Linux systems, particularly Debian-based distributions.</p>
             <figure>
             <img 
               className="titlesubimage"
@@ -18,9 +19,9 @@ function HomePage() {
               alt="A picture of Obed Negron"
             />
             </figure>
-        </div> 
-      </article>
+          </div>
         <ul className="mainpage-buttons-box">
+            <h3>Important Links</h3>
             <a 
               href="/pdf/Resume-ObedNegron.pdf" 
               target="_blank" 
@@ -33,7 +34,7 @@ function HomePage() {
               Collage Portfolio
             </Link>
         </ul>
-              
+      </article>        
     </main>
   );
 }
