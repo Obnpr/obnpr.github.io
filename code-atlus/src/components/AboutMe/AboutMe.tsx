68f8b68acbc9cca2bd56c2113880b468b55185f2
Obed Negron Bodon <kaiserob@icloud.com>
2025-09-15 13:40:23 -0400

    Better aligned the Skills part of AboutMe. Added Additional Skills in addition on AboutMe.

--- code-atlus/src/components/AboutMe/AboutMe.tsx
+++ code-atlus/src/components/AboutMe/AboutMe.tsx
@@ -23,56 +23,123 @@ function AboutMe() {
           </div>
         </article>
         <article className="aboutme-main-content-skills">
-          <h2>Skills</h2>
+          <h2>Skill Set</h2>
           <ul className="aboutme-content-skills-table">
-              <figure>
-                <img
-                  src="/logo/HTML5.webp"
-                  alt="HTML5 Logo"
-                />
-              </figure>
-              <p>HTML</p>
-              <figure>
-                <img
-                  src="/logo/CSS3.webp"
-                  alt="CSS3 Logo"
-                />
-              </figure>
-              <p>CSS</p>
-              <figure>
-                <img
-                  src="/logo/React.webp"
-                  alt="React Logo"
-                />
-              </figure>
-              <p>React</p>
-          </ul>
-
-          <ul className="aboutme-content-skills-table">
-              <figure>
-                <img
-                  src="/logo/C++.webp"
-                  alt="C++ Logo"
-                />
-              </figure>
-              <p>C++</p>
-              <figure>
-                <img
-                  src="/logo/CSharp.webp"
-                  alt="C# Logo"
-                />
-              </figure>
-              <p>C#</p>
-              <figure>
-                <img
-                  src="/logo/Godot.webp"
-                  alt="Godot Engine Logo"
-                />
-              </figure>
-              <p>GDScript</p>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/C++.webp"
+                    alt="C++ Logo"
+                  />
+                </figure>
+                <p>C++</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/Godot.webp"
+                    alt="Godot Engine Logo"
+                  />
+                </figure>
+                <p>GDScript</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/HTML5.webp"
+                    alt="HTML5 Logo"
+                  />
+                </figure>
+                <p>HTML</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/CSS3.webp"
+                    alt="CSS3 Logo"
+                  />
+                </figure>
+                <p>CSS</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/Debian.webp"
+                    alt="Debian Logo"
+                  />
+                </figure>
+                <p>Linux Debian Dist.</p>
+              </li>
           </ul>
         </article>
-
+        <article className="aboutme-main-content-skills">
+          <h2>Additional Skill Set</h2>
+          <ul className="aboutme-content-skills-table">
+              <li>
+                <figure>
+                  <img
+                    src="/logo/CSharp.webp"
+                    alt="C# Logo"
+                  />
+                </figure>
+                <p>C#</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/DotNet.webp"
+                    alt="Dot Net Logo"
+                  />
+                </figure>
+                <p>.NET</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/Python.webp"
+                    alt="Python Logo"
+                  />
+                </figure>
+                <p>Python</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/React.webp"
+                    alt="React Logo"
+                  />
+                </figure>
+                <p>React</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/Javascript.webp"
+                    alt="Javascript Logo"
+                  />
+                </figure>
+                <p>Javascript</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/Docker.webp"
+                    alt="Docker Container Logo"
+                  />
+                </figure>
+                <p>Docker</p>
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/logo/Ubiquiti.webp"
+                    alt="Ubiquiti Logo"
+                  />
+                </figure>
+                <p>Ubiquiti Products</p>
+              </li>
+          </ul>
+        </article>
       </main>
   );
 }
