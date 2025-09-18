d0bbbe709615ade3ccab891002a1591db5f5ea73
Obed Negron Bodon <kaiserob@icloud.com>
2025-09-17 21:20:01 -0400

    Sidebar fixed on mobile. Started to fix Portfolio messy div mess.

--- code-atlus/src/components/Portfolio/Portfolio.tsx
+++ code-atlus/src/components/Portfolio/Portfolio.tsx
@@ -5,7 +5,6 @@ import './PortfolioMobile.css';
 const Portfolio: React.FC = () => {
 
   return (
-    <>
       <main className="main-content-box">
         <div className="main-content">
           <h1 id="Portfolio" >Portfolio</h1>
@@ -54,49 +53,35 @@ const Portfolio: React.FC = () => {
                 </div>
             </div>
           </ul>
-          <h2 className="title-before-content">Past Projects</h2>
-          <ul className="portfolio-article-container">
-            <div className="portfolio-article-box">
-              <img
-                src="./placeholder/game-image.webp"
-                alt="Placeholder image of a game stick"
-              />
-              <img
-                className="portfolio-article-box-logo"
-                src="./logo/fuelzone-logo.webp"
-                alt="Logo of Fuelzone"
-              />
-                <h3>FuelZone</h3>
-                <p>Is a Blogging website with interactive user like and comenting system. Build from .NET w/ C#, HTML, CSS and Javascript</p>
+          
+          <h2 className="portfolio-table-title">Past Projects</h2>
+          <ul className="portfolio-table-container">
+              <li className="portfolio-table-article">
+                <img
+                  src="./placeholder/game-image.webp"
+                  alt="Placeholder image of a game stick"
+                />
+                <div className="portfolio-table-article-logo-title">
+                  <figure>
+                    <img
+                      className="portfolio-table-article-logo"
+                      src="./logo/fuelzone-logo.webp"
+                      alt="Logo of Fuelzone"
+                    />
+                  </figure>
+                  <h3>FuelZone</h3>
+                </div>
+                <p>
+                  Is a Blogging website with interactive user-led like and commenting system. Build utilizing .NET, C#, HTML, CSS and Javascript.
+                </p>
                 <p>Not hosted</p>
-              <div className="portfolio-article-button-alignment">
-                <a
-                  href="https://github.com/Obnpr/Fuelzone-Blogs" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  Github
-                </a>
-                <a
-                  id="is-disabled"
-                >
-                  View More
-                </a>
-              </div>
-            </div>
-            <div className="portfolio-article-box">
-              <img
-                src="./placeholder/game-image.webp"
-                alt="A picture of Obed Negron"
-              />
-                <h3>N/A</h3>
-                <p>N/A</p>
-                <p>Platform: N/A</p>
-                <div className="portfolio-article-button-alignment">
+                <div className="portfolio-table-article-buttons[portfolio-article-button-alignment">
                   <a
-                    id="is-disabled"
+                    href="https://github.com/Obnpr/Fuelzone-Blogs" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
-                    Github
+                    View Source
                   </a>
                   <a
                     id="is-disabled"
@@ -104,11 +89,10 @@ const Portfolio: React.FC = () => {
                     View More
                   </a>
                 </div>
-            </div>
+              </li>
           </ul>
         </div>
       </main>
-    </>
   );
 };
 
